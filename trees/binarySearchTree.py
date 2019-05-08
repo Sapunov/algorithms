ebd273ae6b320eb8f937de61dbc18ebf2887ddcf
Nikita Sapunov <kiton1994@gmail.com>
2019-05-08 22:00:14 +0300

    Next method and insert_all

--- trees/binarySearchTree.py
+++ trees/binarySearchTree.py
@@ -7,14 +7,64 @@ class Node:
         self.left = left
         self.right = right
 
+    def has_right_child(self):
+
+        return self.right is not None
+
+    def has_left_child(self):
+
+        return self.left is not None
+
+    def is_left_child(self):
+
+        return self.parent and self.parent.left == self
+
+    def is_right_child(self):
+
+        return self.parent and self.parent.right == self
+
+    def is_root(self):
+
+        return not self.parent
+
     def __repr__(self):
 
         return f'<Node:{self.key}>'
 
+    def __str__(self):
+
+        return self.__repr__()
+
 
 def assert_node(node):
 
-    assert isinstance(node, Node), 'Node must be an instance of class Node'
+    assert isinstance(node, Node), \
+        f'Node must be an instance of class Node. Passed: {type(node)}'
+
+
+def right_ancestor(node):
+
+    assert_node(node)
+
+    if node.parent is None:
+        return None
+
+    if node.key < node.parent.key:
+        return node.parent
+    else:
+        return right_ancestor(node.parent)
+
+
+def left_descendant(node):
+    '''Самая левая нода, начиная с node
+    '''
+
+    assert_node(node)
+
+    if node.left is None:
+        return node
+    else:
+        return left_descendant(node.left)
 
 
 class BinarySearchTree:
@@ -22,6 +72,7 @@ class BinarySearchTree:
     def __init__(self, root_node=None):
 
         self.root = root_node
+        self.size = 0
 
     def find(self, key):
 
@@ -45,6 +96,12 @@ class BinarySearchTree:
                     parent.right = node
             else:  # root node
                 self.root = node
+            self.size += 1
+
+    def insert_all(self, keys):
+
+        for key in keys:
+            self.insert(key)
 
     def delete(self, node):
 
@@ -54,7 +111,11 @@ class BinarySearchTree:
     def next(self, node):
 
         assert_node(node)
-        raise NotImplementedError()
+
+        if node.has_right_child():
+            return left_descendant(node.right)
+        else:
+            return right_ancestor(node)
 
     def find_range(self, left, right):
 
@@ -76,20 +137,18 @@ class BinarySearchTree:
             if node.key == key:
                 return (True, node)
             if node.key > key:
-                if node.left is None:
+                if not node.has_left_child():
                     return (False, node)
                 node = node.left
             else:
-                if node.right is None:
+                if not node.has_right_child():
                     return (False, node)
                 node = node.right
 
-    def _right_ancestor(self, node):
+    def __repr__(self):
 
-        assert_node(node)
-        raise NotImplementedError()
+        return f'<BST: root={self.root} size={self.size}>'
 
-    def _left_descendant(self, node):
+    def __str__(self):
 
-        assert_node(node)
-        raise NotImplementedError()
+        return self.__repr__()
